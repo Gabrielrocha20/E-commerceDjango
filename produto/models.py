300a78d2b0f879fe028179cf075563f488b222cd
Gabriel Rocha <gabrielrocha12.gs@gmail.com>
2022-08-04 13:21:45 -0300

    Atualizaçao da parte da area do adm

--- produto/models.py
+++ produto/models.py
@@ -1,3 +1,4 @@
+from pickletools import optimize
 from django.conf import settings
 import os
 from PIL import Image
@@ -24,7 +25,16 @@ class Produto(models.Model):
         img_pil = Image.open(img_full_path)
         original_width, original_height = img_pil.size
 
-        print(original_width, original_height)
+        if original_width <= new_with:
+            img_pil.close()
+            return
+        new_height = round((new_with * original_height) / original_width)
+        new_img = img_pil.resize((new_with, new_height), Image.LANCZOS)
+        new_img.save(
+            img_full_path,
+            optimize=True,
+            quality=50
+        )
 
     def save(self, *args, **kwargs):
         super().save(*args, **kwargs)
@@ -38,11 +48,16 @@ class Produto(models.Model):
         return self.nome
 
 
-'''
-        Variacao:
-            nome - char
-            produto - FK Produto
-            preco - Float
-            preco_promocional - Float
-            estoque - Int
-'''
+class Variacao(models.Model):
+    produto = models.ForeignKey(Produto, on_delete=models.CASCADE)
+    nome = models.CharField(max_length=50, blank=True, null=True)
+    preco = models.FloatField()
+    preco_promocional = models.FloatField(default=0)
+    estoque = models.PositiveIntegerField(default=1)
+
+    def __str__(self):
+        return self.nome or self.produto.nome
+
+    class Meta:
+        verbose_name = 'Variação'
+        verbose_name_plural = 'Variações'
